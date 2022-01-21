4cb2ac31eb02bc89d130bbacddbe67a756eafc14
victorMayer <victor.mayer@outlook.com>
2022-01-21 16:11:46 -0300

    subcomponents selection logic of payment section implemented

---
+++ src/pages/Dashboard/Payment/ReviewPayment.js
@@ -0,0 +1,5 @@
+export default function ReviewPayment() {
+  return (
+    <>Você já pagou</>
+  );
+};
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,5 +1,8 @@
 import PaymentScreen from "./PaymentScreen";
-import { Title,  } from "../../../components/_shared/Texts";
+import PreviousSectionNotCompleted from "./PreviousSectionNotCompleted";
+import ReviewPayment from "./ReviewPayment";
+import SelectIngress from "./SelectIngress";
+import { Title, } from "../../../components/_shared/Texts";
 import { useEffect, useState } from "react";
 import useApi from "../../../hooks/useApi";
 
@@ -7,29 +10,30 @@ export default function Payment() {
   const { enrollment } = useApi();
 
   const [ hasEnrollment, setHasEnrollment ] = useState(true);
+  const [ hasIngress, setHasIngress ] = useState(undefined);
   const [ paymentConfirmed, setPaymentConfirmed ] = useState(undefined);
   
   useEffect(() => {
-    // check if this id has payment or ingress
     enrollment.getPersonalInformations().then(answer => {
-      console.log(answer.data); // delete this line later
-      const { isOnlinePlan, hasHotel, payentConfirmed, userId } = answer.data;
+      const { isOnlinePlan, hasHotel, payentConfirmed, } = answer.data;
       
-      if (!userId) setHasEnrollment(false);
-
-      if (paymentConfirmed === null) setPaymentConfirmed(null);
-      else if (paymentConfirmed === false) setPaymentConfirmed(false);
-      else if (paymentConfirmed === true) setPaymentConfirmed(true);
+      if (!answer.data) setHasEnrollment(false);
+      if (hasHotel !== null && isOnlinePlan !== null) setHasIngress(true);
+      if (payentConfirmed === null || payentConfirmed === false) setPaymentConfirmed(false);
+      if (payentConfirmed) setPaymentConfirmed(true);
     }).catch();
   }, []);
 
-  const renderEnrollment = hasEnrollment ? <></>/*checkar estagio da inscrição*/ : /*<PreviousSectionNotCompleted />*/ "";
-  const payment = paymentConfirmed ? /*<ReviewPayment />*/<></> : <PaymentScreen />;
+  const payment = paymentConfirmed ? <ReviewPayment /> : <PaymentScreen />;
+  const ingress = hasIngress ? payment : <SelectIngress />;
 
   return <div>
     <Title>Ingresso e pagamento</Title>
-    
-    {paymentConfirmed === null ? renderEnrollment : payment }
 
+    { hasEnrollment 
+      ?  ingress
+      : <PreviousSectionNotCompleted /> 
+    }
+    
   </div>;
 };
