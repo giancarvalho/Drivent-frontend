4c85b8a72a5fc493490f26d496fa8873f1f02f0c
victorMayer <victor.mayer@outlook.com>
2022-01-21 12:47:55 -0300

    implementing subcomponent selection inside payment page

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,9 +1,35 @@
 import PaymentScreen from "./PaymentScreen";
-import { Title } from "../../../components/_shared/Texts";
+import { Title,  } from "../../../components/_shared/Texts";
+import { useEffect, useState } from "react";
+import useApi from "../../../hooks/useApi";
 
 export default function Payment() {
+  const { enrollment } = useApi();
+
+  const [ hasEnrollment, setHasEnrollment ] = useState(true);
+  const [ paymentConfirmed, setPaymentConfirmed ] = useState(undefined);
+  
+  useEffect(() => {
+    // check if this id has payment or ingress
+    enrollment.getPersonalInformations().then(answer => {
+      console.log(answer.data); // delete this line later
+      const { isOnlinePlan, hasHotel, payentConfirmed, userId } = answer.data;
+      
+      if (!userId) setHasEnrollment(false);
+
+      if (paymentConfirmed === null) setPaymentConfirmed(null);
+      else if (paymentConfirmed === false) setPaymentConfirmed(false);
+      else if (paymentConfirmed === true) setPaymentConfirmed(true);
+    }).catch();
+  }, []);
+
+  const renderEnrollment = hasEnrollment ? <></>/*checkar estagio da inscrição*/ : /*<PreviousSectionNotCompleted />*/ "";
+  const payment = paymentConfirmed ? /*<ReviewPayment />*/<></> : <PaymentScreen />;
+
   return <div>
-    <Title>Ingresso e Pagamento</Title>
-    <PaymentScreen />
+    <Title>Ingresso e pagamento</Title>
+    
+    {paymentConfirmed === null ? renderEnrollment : payment }
+
   </div>;
-}
+};
