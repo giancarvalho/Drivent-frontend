2f213514ea275aa1a3f7a528872773928418e8f4
LeoDias-0 <leodiasleodias00@gmail.com>
2022-01-31 12:13:27 -0300

    minor fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  //  baseURL: process.env.REACT_APP_API_BASE_URL
-  baseURL: "http://localhost:4000"
+  baseURL: process.env.REACT_APP_API_BASE_URL
 });
 
 export default instance;
